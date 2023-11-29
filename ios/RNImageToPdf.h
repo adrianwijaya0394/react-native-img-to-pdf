369f7abe19a16fdfdd2eb85f2eba62aaeb673fde
star <65936251+adrianwijaya0394@users.noreply.github.com>
2023-11-29 13:44:20 +0700

    ios

---
+++ ios/RNImageToPdf.h
@@ -0,0 +1,10 @@
+#import <React/RCTView.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTUtils.h>
+#import <React/RCTConvert.h>
+#import <React/RCTEventDispatcher.h>
+
+@interface RNImageToPdf : NSObject <RCTBridgeModule>
+
+@end
+  
